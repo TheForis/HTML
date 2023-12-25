badeb4a0998309000483fa20d193c965657e9e59
TheForis <boriskrstovski@gmail.com>
2023-12-25 03:33:24 +0100

    1

--- JAVASCRIPT/8th class cont/10th exercise/script.js
+++ JAVASCRIPT/8th class cont/10th exercise/script.js
@@ -1,15 +1,27 @@
-
+let whoInput = document.getElementById('who').value;
+let whatInput = document.getElementById('what').value;
+let whenInput = document.getElementById('when').value;
+let button = document.getElementById('button');
+let result = document.getElementById('paragraph');
 
 
 
 function storyTeller (who, what, when) {
 
-    let story = `Meet ${who}. ${who} job is: ${what}. ${when} is the best time to be alive`;
-    
-    return story;
+    let result = `Meet ${who}. ${who} is working as: ${what}. ${when} is the best time to be alive`;
+    return result;
      
 }
- 
- 
 
- console.log(storyResult);
+button.addEventListener('click', function(){
+    let whoInput = document.getElementById('who').value;
+    let whatInput = document.getElementById('what').value;
+    let whenInput = document.getElementById('when').value;
+    
+    let storyResult = storyTeller(whoInput,whatInput, whenInput);
+    console.log(storyResult);
+    paragraph.innerText = storyResult
+
+})
+
+ 
