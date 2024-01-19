86e236eed7e8001b127d56e2de04e46d2014e84a
TheForis <boriskrstovski@gmail.com>
2024-01-19 01:35:10 +0100

    1

--- JAVASCRIPT ADVANCED/Homework/3rd class homework/task2/script.js
+++ JAVASCRIPT ADVANCED/Homework/3rd class homework/task2/script.js
@@ -25,19 +25,59 @@ function printInfo(array){
     }
     else {
     for(let i=0; i < array.length; i++){
-        console.log(array);
+        let row = document.createElement('tr');
         
-    }}
+        for (let cells = 0; cells < 3; cells++ ){
+            if (cells == 0){
+                let cell = document.createElement('td');
+                let cellText = document.createTextNode(array[i].title);
+                cell.appendChild(cellText);
+                row.appendChild(cell);
+                cell.style.color = array[i].color
+            }
+            else if (cells == 1){
+                let cell = document.createElement('td');
+                let cellText = document.createTextNode(array[i].priority);
+                cell.appendChild(cellText);
+                row.appendChild(cell);
+            }
+            else if (cells == 2){
+                let cell = document.createElement('td');
+                let cellText = document.createTextNode(array[i].description);
+                cell.appendChild(cellText);
+                row.appendChild(cell);
+        }
+
+        }
+        tableBody.appendChild(row);
+    }
+
+
+
+}
 }
 
 saveReminderButton.addEventListener('click', function(){
+    if (reminderTitle.value == '' || reminderDescription.value == '' || reminderPriority.value == '' ){
+        alert('Please fill out every field!')
+    }
+    else {
     let reminder = new Reminder (reminderTitle.value, reminderPriority.value, reminderColor.value, reminderDescription.value);
     reminderContainer.push(reminder);
+    }
 
-console.log(reminderContainer);
+
+
+
+    reminderTitle.value = '';
+    reminderPriority.value = '';
+    reminderColor.value = '#000000';
+    reminderDescription.value = '';
+
 })
 
 
 showRemindersButton.addEventListener('click', function(){
+    tableBody.innerHTML = ''
     printInfo(reminderContainer);
 })
